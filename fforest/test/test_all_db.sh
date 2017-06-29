142357a0aa951295734285fdecf642948042862f
NicolasBi <nicolasbizzozzero@gmail.com>
2017-06-29 17:38:20 +0200

    Updated tests files

--- fforest/test/test_all_db.sh
+++ fforest/test/test_all_db.sh
@@ -1,4 +1,6 @@
+python3 ../../setup.py install
 rm -rf bank bank-additional adult australian segment yeast
+reset
 fforest data/bank.csv --delimiter-input=';' --trees-in-forest 15 --have-header --class y
 fforest data/bank-additional.csv --delimiter-input=';' --trees-in-forest 15 --have-header --class y
 fforest data/adult.data --delimiter-input=',' --quoting-input none --trees-in-forest 15 --class -1
--- fforest/test/test_bank.sh
+++ fforest/test/test_bank.sh
@@ -1,2 +1,4 @@
+python3 ../../setup.py install
 rm -rf bank
+reset
 fforest data/bank.csv --delimiter-input=';' --trees-in-forest 15 --have-header --class y
