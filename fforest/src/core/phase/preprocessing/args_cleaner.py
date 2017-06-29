7d3697a77f303ec1a804d37b994edadcb9a5a865
NicolasBi <nicolasbizzozzero@gmail.com>
2017-06-29 15:36:22 +0200

    Parameter destination directory works like a charm.

--- fforest/src/core/phase/preprocessing/args_cleaner.py
+++ fforest/src/core/phase/preprocessing/args_cleaner.py
@@ -14,7 +14,7 @@ from fforest.src.file_tools.csv_tools import find_index_for_class, index_in_boun
 from fforest.src.file_tools.csv_tools import str_to_quoting
 from fforest.src.file_tools.format import string_to_format
 from fforest.src.getters.get_output_message import string_to_verbosity
-from fforest.src.vrac.file_system import get_filename, get_path
+from fforest.src.vrac.file_system import get_filename, get_absolute_path
 from fforest.src.vrac.maths import is_a_percentage, is_an_int
 
 
@@ -61,9 +61,9 @@ def clean_args(args: dict) -> None:
     for param_name in args.keys():
         if param_name == gpn.parent_dir():
             if not args[param_name]:
-                args[param_name] = "."
+                args[param_name] = get_absolute_path(".")
             else:
-                args[param_name] = get_path(args[param_name])
+                args[param_name] = get_absolute_path(args[param_name])
         if param_name == gpn.class_name():
             _check_key_exists(args, param_name, custom_exception=MissingClassificationAttribute)
             _clean_column_index_or_name(args=args, param_name=param_name, column_name="class")
