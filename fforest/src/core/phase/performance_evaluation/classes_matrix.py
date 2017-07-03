a2587ee1bad0afd6fbf7e459f664f523162ffd4c
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-04 01:16:02 +0200

    Started creating classes_matrix module

---
+++ fforest/src/core/phase/performance_evaluation/classes_matrix.py
@@ -0,0 +1,8 @@
+""" This module will create a directory and compute a "class matrix" for each class and for each triangular norm.
+A class matrix z
+
+"""
+
+
+def classes_matrix() -> None:
+    pass
