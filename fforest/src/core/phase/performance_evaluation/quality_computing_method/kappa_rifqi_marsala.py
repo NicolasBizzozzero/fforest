f668f0422928b98bd1748c920613e498610ca967
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-06 14:32:51 +0200

    Modified _get_tree_quality method

--- fforest/src/core/phase/performance_evaluation/quality_computing_method/kappa_rifqi_marsala.py
+++ fforest/src/core/phase/performance_evaluation/quality_computing_method/kappa_rifqi_marsala.py
@@ -31,7 +31,6 @@ def kappa_rifqi_marsala() -> Dict[str, Dict[str, float]]:
         forest_quality_all_tnorms[tnorm] = \
             _get_forest_quality(reference_database_path=env.reference_database_path,
                                 subsubtrain_directories_path=env.subsubtrain_directories_path,
-                                difficulty_vector_path=env.difficulty_vectors_paths[tnorm],
                                 salammbo_vector_paths=env.salammbo_vectors_paths[tnorm],
                                 dialect=env.dialect_output)
     return forest_quality_all_tnorms
@@ -62,58 +61,31 @@ def _load_instances_difficulty(difficulty_vector_path: str, instances: Dict[str,
         instances[identifier][KEY_DIFFICULTY] = float(difficulty)
 
 
-def _get_forest_quality(reference_database_path: str, subsubtrain_directories_path: str, difficulty_vector_path: str,
+def _get_forest_quality(reference_database_path: str, subsubtrain_directories_path: str,
                         salammbo_vector_paths: str, dialect: Dialect) -> Dict[str, float]:
-    instances_identifiers = get_column(path=reference_database_path, column=0, have_header=False, dialect=dialect)
     forest_quality = dict()
     for tree_index, tree in enumerate(subsubtrain_directories_path):
         forest_quality[tree] = _get_tree_quality(instances_identifiers=instances_identifiers,
-                                                 difficulty_vector_path=difficulty_vector_path,
                                                  salammbo_vector_path=salammbo_vector_paths[tree_index],
                                                  dialect=dialect)
     return forest_quality
 
 
-def _get_tree_quality(difficulty_vector_path: str, salammbo_vector_path: str, instances_identifiers: List[str],
-                      dialect: Dialect) -> float:
-    return sum(_get_instance_score(instance_identifier=instance,
-                                   difficulty_vector_path=difficulty_vector_path,
-                                   salammbo_vector_path=salammbo_vector_path,
-                                   dialect=dialect) for instance in instances_identifiers) / len(instances_identifiers)
+def _get_tree_quality(instances: Dict[str, Dict[str, Union[str, float]]]) -> float:
+    """ Return the quality of a tree.
+    The quality of a tree, as defined by the Kappa-Rifqi-Marsala method, is the sum of the score of all instances
+    divided by the number of instances (to normalize the result).
+    """
+    return sum(_get_instance_score(difficulty=instances[identifier][KEY_DIFFICULTY],
+                                   membership=instances[identifier][KEY_MEMBERSHIP]) for
+               identifier in instances.keys()) / len(instances)
 
 
-def _get_instance_score(instance_identifier: str, difficulty_vector_path: str, salammbo_vector_path: str,
-                        dialect: Dialect) -> float:
+def _get_instance_score(difficulty: float, membership: float) -> float:
     """ Return the score of an instance.
     An instance's score is the product of its difficulty by the % of membership found by the tree.
     """
-    true_class = _get_instance_trueclass(instance_identifier=instance_identifier,
-                                         salammbo_vector_path=salammbo_vector_path,
-                                         dialect=dialect)
-    difficulty = _get_instance_difficulty(instance_identifier=instance_identifier,
-                                          difficulty_vector_path=difficulty_vector_path,
-                                          dialect=dialect)
-    membership = _get_instance_membership(instance_identifier=instance_identifier,
-                                          salammbo_vector_path=salammbo_vector_path,
-                                          true_class=true_class,
-                                          dialect=dialect)
     return difficulty * membership
-
-
-def _get_instance_trueclass(instance_identifier: str, salammbo_vector_path: str, dialect: Dialect) -> str:
-    row = get_identified_row(path=salammbo_vector_path,
-                             identifier_name=KEY_ID,
-                             row_id=instance_identifier,
-                             dialect=dialect)
-    return row[KEY_TRUECLASS]
-
-
-def _get_instance_difficulty(instance_identifier: str, difficulty_vector_path: str, dialect: Dialect) -> float:
-    row = get_identified_row(path=difficulty_vector_path,
-                             identifier_name=KEY_ID,
-                             row_id=instance_identifier,
-                             dialect=dialect)
-    return float(row[KEY_DIFFICULTY])
 
 
 def _get_instance_membership(instance_identifier: str, salammbo_vector_path: str, true_class: str,
