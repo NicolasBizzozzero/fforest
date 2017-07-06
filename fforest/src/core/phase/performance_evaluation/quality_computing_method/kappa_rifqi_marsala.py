91778081def3ff14be7e1c6d56763ede05e027f8
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-06 15:43:06 +0200

    Removing that green stuff

--- fforest/src/core/phase/performance_evaluation/quality_computing_method/kappa_rifqi_marsala.py
+++ fforest/src/core/phase/performance_evaluation/quality_computing_method/kappa_rifqi_marsala.py
@@ -5,13 +5,17 @@ An instance's score is the product of its difficulty by the % of membership foun
 from typing import Dict, List, Union
 
 from fforest.src.core.phase.learning_process.forest_construction import KEY_ID, KEY_TRUECLASS, KEY_DIFFICULTY
-from fforest.src.file_tools.csv_tools import get_identified_row, get_column, get_columns
+from fforest.src.file_tools.csv_tools import get_identified_row, get_column, get_columns, iter_rows, iter_rows_dict
 from fforest.src.file_tools.dialect import Dialect
 import fforest.src.getters.environment as env
 from fforest.src.vrac.decorators import timeit
 
 
 KEY_MEMBERSHIP = "%membership"
+
+
+# TODO: Check if salammbo and difficulty vectors have headers
+# TODO: _load_instances_id_and_trueclass can probably be removed
 
 
 def kappa_rifqi_marsala() -> Dict[str, Dict[str, float]]:
@@ -53,7 +57,7 @@ def _load_instances_id_and_trueclass(salammbo_vector_path: str, dialect: Dialect
 
 def _load_instances_difficulty(difficulty_vector_path: str, instances: Dict[str, Dict[str, str]],
                                dialect: Dialect) -> None:
-    """ Get the instances' difficulty from a their difficulty vector. """
+    """ Get the instances' difficulty from their difficulty vector. """
     for identifier, difficulty in get_columns(path=difficulty_vector_path,
                                               columns=[KEY_ID, KEY_DIFFICULTY],
                                               have_header=True,
@@ -61,13 +65,33 @@ def _load_instances_difficulty(difficulty_vector_path: str, instances: Dict[str,
         instances[identifier][KEY_DIFFICULTY] = float(difficulty)
 
 
-def _get_forest_quality(reference_database_path: str, subsubtrain_directories_path: str,
+def _load_instances_membership(salammbo_vector_path: str, instances: Dict[str, Dict[str, str]], true_class: str,
+                               dialect: Dialect) -> None:
+    """ Get the instances' membership from their salammbo vector. """
+    for row in iter_rows_dict(path=salammbo_vector_path, dialect=dialect):
+        identifier, true_class = row[KEY_ID], row[KEY_TRUECLASS]
+        membership = row[true_class]
+        instances[identifier][KEY_MEMBERSHIP] = membership
+
+    for identifier, membership in get_columns(path=salammbo_vector_path,
+                                              columns=[KEY_ID, true_class],
+                                              have_header=True,
+                                              dialect=dialect):
+        instances[identifier][KEY_MEMBERSHIP] = float(membership)
+
+
+def _get_forest_quality(instances: Dict[str, Dict[str, Union[str, float]]], subsubtrain_directories_path: str,
                         salammbo_vector_paths: str, dialect: Dialect) -> Dict[str, float]:
+    """ Compute the forest quality.
+    Return a dictionary mapping each forest's path to their quality.
+    """
     forest_quality = dict()
-    for tree_index, tree in enumerate(subsubtrain_directories_path):
-        forest_quality[tree] = _get_tree_quality(instances_identifiers=instances_identifiers,
-                                                 salammbo_vector_path=salammbo_vector_paths[tree_index],
-                                                 dialect=dialect)
+    for tree_path in subsubtrain_directories_path:
+        _load_instances_membership(salammbo_vector_path=,
+                                   instances=instances,
+                                   true_class=,
+                                   dialect=dialect)
+        forest_quality[tree_path] = _get_tree_quality(instances=instances)
     return forest_quality
 
 
