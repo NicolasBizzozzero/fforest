e7444cb177bd45158fada759aa17cf098d50811f
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-17 19:10:39 +0200

    Removed all statements related to --first-phase

--- fforest/src/core/phase/preprocessing/preparsing.py
+++ fforest/src/core/phase/preprocessing/preparsing.py
@@ -11,15 +11,10 @@ def compute_first_phase() -> Phase:
     main_dir_name = _get_main_dir_name()
     environment_file_path = os.path.join(main_dir_name, ENVIRONMENT_FILE_NAME)
 
-    if _env_file_exists(environment_file_path):
-        # User wants to resume where he stopped last time
-        if _starting_phase_asked():
-            load_environment_file(path=environment_file_path)
-            if
-            return env.current_phase
-        elif _resume_phase_asked():
-            load_environment_file(path=environment_file_path)
-            return env.current_phase
+    # User wants to resume where he stopped last time
+    if _env_file_exists(environment_file_path) and _resume_phase_asked():
+        load_environment_file(path=environment_file_path)
+        return env.current_phase
     # User want to compute all phases, regarding of previous computations
     else:
         return Phase.PARSING
