60591498bcdbbd6bdb7b7908ce33fcfa14f56001
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-17 13:55:31 +0200

    Preparsing module completed

--- fforest/src/core/phase/preprocessing/preparsing.py
+++ fforest/src/core/phase/preprocessing/preparsing.py
@@ -11,17 +11,21 @@ def compute_first_phase() -> Phase:
     main_dir_name = _get_main_dir_name()
     environment_file_path = os.path.join(main_dir_name, ENVIRONMENT_FILE_NAME)
 
-    if _env_file_exists(environment_file_path=environment_file_path):
+    # User wants to resume where he stopped last time
+    if _env_file_exists(environment_file_path) and _resume_phase_asked():
         load_environment_file(path=environment_file_path)
-        resume_phase(env.current_phase)
-
-
-def _get_main_dir_name() -> str:
-    return sys.argv[sys.argv.index(gpn.main_directory()) + 1]
+        return env.current_phase
+    # User want to compute all phases, regarding of previous computations
+    else:
+        return Phase.NONE
 
 
 def _env_file_exists(environment_file_path: str) -> bool:
     return file_exists(environment_file_path)
+
+
+def _get_main_dir_name() -> str:
+    return sys.argv[sys.argv.index(gpn.main_directory()) + 1]
 
 
 def _resume_phase_asked() -> bool:
