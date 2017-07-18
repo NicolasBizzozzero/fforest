4d906e9dfe43c33e20464561f4bda1a7becf7132
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-18 15:42:24 +0200

    Updated main module

--- fforest/main.py
+++ fforest/main.py
@@ -1,11 +1,7 @@
-from fforest.src.core.phase.ending.ending import failure_safe, ending
-from fforest.src.core.phase.learning_process.forest_construction import forest_construction
-from fforest.src.core.phase.learning_process.forest_reduction import forest_reduction
+from fforest.src.core.phase.ending.ending import failure_safe
 from fforest.src.core.phase.phase import call_all_phases
 from fforest.src.core.phase.preprocessing.args_parser import parse_args_main_entry_point
 from fforest.src.core.phase.preprocessing.preparsing import compute_first_phase
-from fforest.src.core.phase.preprocessing.preprocessing import preprocessing
-import fforest.src.getters.environment as env
 
 
 @failure_safe
@@ -15,23 +11,40 @@ def main_entry_point() -> None:
                     parsing_function=parse_args_main_entry_point)
 
 
+def parsing_entry_point() -> None:
+    pass
+
+
 def preprocessing_entry_point() -> None:
-    preprocessing()
-    ending()
+    pass
 
 
-def initialization_entry_point() -> None:
+def initial_split_entry_point() -> None:
+    pass
+
+
+def reference_split_entry_point() -> None:
+    pass
+
+
+def subsubtrain_split_entry_point() -> None:
     pass
 
 
 def learning_entry_point() -> None:
-    forest_construction()
-    ending()
+    pass
 
 
 def reduction_entry_point() -> None:
-    forest_reduction()
-    ending()
+    pass
+
+
+def quality_entry_point() -> None:
+    pass
+
+
+def classes_matrices_entry_point() -> None:
+    pass
 
 
 if __name__ == "__main__":
