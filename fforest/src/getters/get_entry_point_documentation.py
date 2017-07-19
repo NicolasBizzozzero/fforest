06183f137013292567a6c3ec4ee3ea5732f94d4b
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-19 15:02:26 +0200

    Implemented all entry_points getter

--- fforest/src/getters/get_entry_point_documentation.py
+++ fforest/src/getters/get_entry_point_documentation.py
@@ -1,7 +1,7 @@
-import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import parse as xml_parse
 
 
-_FILE_PATH = "entry_points.xml"
+_FILE_PATH = "../../res/entry_points.xml"
 _KEY_NAME = "name"
 _KEY_DOCUMENTATION = "documentation"
 
@@ -9,19 +9,51 @@ _KEY_DOCUMENTATION = "documentation"
 def _get_root():
     global _FILE_PATH
 
-    tree = ET.parse(_FILE_PATH)
+    tree = xml_parse(_FILE_PATH)
     return tree.getroot()
 
 
-def _get_entry_point_documentation(root, entry_point_name: str) -> str:
+def _get_entry_point_documentation(entry_point_name: str) -> str:
     global _KEY_NAME
     global _KEY_DOCUMENTATION
 
+    root = _get_root()
     for entry_point in root:
         if entry_point.attrib[_KEY_NAME] == entry_point_name:
             return entry_point.find(_KEY_DOCUMENTATION).text
 
 
 def main_entry_point() -> str:
-    root = _get_root()
-    return _get_entry_point_documentation(root, "main_entry_point")
+    return _get_entry_point_documentation("main_entry_point")
+
+
+def preprocessing_entry_point() -> str:
+    return _get_entry_point_documentation("preprocessing_entry_point")
+
+
+def initial_split_entry_point() -> str:
+    return _get_entry_point_documentation("initial_split_entry_point")
+
+
+def reference_split_entry_point() -> str:
+    return _get_entry_point_documentation("reference_split_entry_point")
+
+
+def subsubtrain_split_entry_point() -> str:
+    return _get_entry_point_documentation("subsubtrain_split_entry_point")
+
+
+def learning_entry_point() -> str:
+    return _get_entry_point_documentation("learning_entry_point")
+
+
+def reduction_entry_point() -> str:
+    return _get_entry_point_documentation("reduction_entry_point")
+
+
+def quality_entry_point() -> str:
+    return _get_entry_point_documentation("quality_entry_point")
+
+
+def classes_matrices_entry_point() -> str:
+    return _get_entry_point_documentation("classes_matrices_entry_point")
