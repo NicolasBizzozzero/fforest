d3db1c45a7115534a9b2cd76630582ff64bf489c
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-23 23:25:31 +0200

    Updated triangular_norm module's documentation

--- fforest/src/core/phase/learning_process/triangular_norms.py
+++ fforest/src/core/phase/learning_process/triangular_norms.py
@@ -1,21 +1,23 @@
-from enum import IntEnum
+""" This module contains useful tools to manipulate the `TriangularNorm` class.
+A T-Norm, or Triangular Norm, is a binary operation used in Fuzzy Logic and which generalize a conjunction.
+It it used by the 'Salammbô' software.
+"""
+import enum
 
 
 KEY_DEFAULT_METHOD = "tnorm_"
 
 
-def tnorm_to_str(tnorm_number: int) -> str:
-    if tnorm_number == TriangularNorm.CLASSIC.value:
-        return "classic"
-    elif tnorm_number == TriangularNorm.ZADEH.value:
-        return "zadeh"
-    elif tnorm_number == TriangularNorm.LUKA.value:
-        return "luka"
-    else:
-        return KEY_DEFAULT_METHOD + str(tnorm_number)
-
-
-class TriangularNorm(IntEnum):
+@enum.unique
+class TriangularNorm(enum.IntEnum):
     CLASSIC = 0
     ZADEH = 1
     LUKA = 2
+
+
+def tnorm_to_str(tnorm_number: int) -> str:
+    """ Return the name of a Triangular Norm number as a lowercase str. """
+    try:
+        return TriangularNorm(tnorm_number).name.lower()
+    except ValueError:
+        return KEY_DEFAULT_METHOD + str(tnorm_number)
