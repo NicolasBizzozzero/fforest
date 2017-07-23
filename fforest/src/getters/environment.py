27c1bcbc6e14115178ef12611011acf6bec1ab5d
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-23 12:26:16 +0200

    Updated environment module's documentation

--- fforest/src/getters/environment.py
+++ fforest/src/getters/environment.py
@@ -1,6 +1,7 @@
 """ This module defines environment variables which'll be used globally by all the modules and packages of the
 software.
 """
+
 # Command-line parameters
 cclassified_vector_prefix = None
 class_matrix_prefix = None
