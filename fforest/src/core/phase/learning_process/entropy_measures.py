fc3da8c2ff6db6eb64b2d9967039f837e6769d3e
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-23 23:33:42 +0200

    Updated entropy_measures module's documentation

--- fforest/src/core/phase/learning_process/entropy_measures.py
+++ fforest/src/core/phase/learning_process/entropy_measures.py
@@ -1,21 +1,29 @@
-from enum import IntEnum
+""" This module contains useful tools to manipulate the `EntropyMeasure` class.
+The entropy measure is use to measure diversity inside a set of value. It is used by the 'Salammbô' software.
+"""
+import enum
 
 
-class EntropyMeasure(IntEnum):
-    UNKNOWN = 0
-    SHANNON = 1
+@enum.unique
+class EntropyMeasure(enum.IntEnum):
+    SHANNON = 0
+
+
+class UnknownEntropyMeasure(Exception):
+    def __init__(self, entropy_measure_name: str):
+        Exception.__init__(self, "The entropy measure \"{entropy_measure_name}\" doesn't"
+                                 " exists".format(entropy_measure_name=entropy_measure_name))
 
 
 def str_to_entropymeasure(string: str) -> EntropyMeasure:
+    """ Return the enum value associated with the name `string`, case insensitive. """
     string = string.lower()
-    if string == "shannon":
-        return EntropyMeasure.SHANNON
-    else:
-        return EntropyMeasure.UNKNOWN
+    for entropymeasure_name, entropymeasure_value in zip(EntropyMeasure.__members__.keys(), EntropyMeasure.__members__.values()):
+        if string == entropymeasure_name.lower():
+            return entropymeasure_value
+    raise UnknownEntropyMeasure(string)
 
 
 def entropymeasure_to_str(measure: EntropyMeasure) -> str:
-    if measure == EntropyMeasure.SHANNON:
-        return "shannon"
-    else:
-        return "unknown"
+    """ Return the name of an entropy measure as a lowercase str. """
+    return measure.name.lower()
