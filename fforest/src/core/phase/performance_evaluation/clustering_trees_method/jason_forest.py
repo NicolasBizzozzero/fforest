2fe3c581d3ad21f9bdc3cfd21797e891fc4b3302
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2017-07-25 22:48:34 +0200

    Update jason_forest.py

--- fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
+++ fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
@@ -1,4 +1,15 @@
-
-
 def jason_forest():
     pass
+
+
+def _data_normalization(file_path: str, dialect: Dialect):
+    matrix = _load_matrix(file_path=file_path, dialect=dialect)
+    for attribute in _get_attributes():
+        mean = _compute_mean(matrix=matrix, attribute=attribute)
+        ecart_type = _compute_ecart_type(matrix=matrix, attribute=attribute, mean=mean)
+        for tree_name in matrix.keys():
+            matrix[tree_name][attribute] = (matrix[tree_name][attribute] - mean) / ecart_type
+
+
+def _get_attributes(file_path: str, dialect: Dialect) -> List[str]:
+    return get_header(file_path=file_path, dialect=dialect)[1:]
