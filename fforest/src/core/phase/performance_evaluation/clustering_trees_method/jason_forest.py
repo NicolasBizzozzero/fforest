f62984e922aeb977d90117dbcfe32edd50f7e1c0
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-27 19:05:49 +0200

    Not dumping the """matrix""" anymore (keep the database loaded in memory)

--- fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
+++ fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
@@ -9,41 +9,40 @@ from collections import OrderedDict
 
 
 def jason_forest() -> Dict[str, Dict[str, List[Dict[str, Union[str, int]]]]]:
-    raise NotImplementedError("The values created by this algorithms are FALSE")
     clustering_trees = dict()
     for class_name in env.possible_classes:
         for tnorm_name in env.t_norms_names:
-            _data_normalization(file_path=env.classes_matrices_files_paths[class_name][tnorm_name],
-                                output_path=env.clustering_trees_files_paths[class_name][tnorm_name],
-                                dialect=env.dialect_output)
+            database = _data_normalization(file_path=env.classes_matrices_files_paths[class_name][tnorm_name],
+                                           output_path=env.clustering_trees_files_paths[class_name][tnorm_name],
+                                           dialect=env.dialect_output)
     return clustering_trees
 
 
-def _data_normalization(file_path: str, output_path: str, dialect: Dialect) -> None:
-    matrix = _load_matrix(file_path=file_path, dialect=dialect)
+def _data_normalization(file_path: str, output_path: str, dialect: Dialect) -> Dict[str, Dict[str, float]]:
+    database = _load_database(file_path=file_path, dialect=dialect)
     for attribute in _get_attributes(file_path=file_path, dialect=dialect):
-        mean = _compute_mean(matrix=matrix, attribute=attribute)
+        mean = _compute_mean(database=database, attribute=attribute)
         print("mean:", mean)
-        standard_deviation = _compute_standard_deviation(matrix=matrix, attribute=attribute)
+        standard_deviation = _compute_standard_deviation(database=database, attribute=attribute)
         print("sd:", standard_deviation)
-        for tree_name in matrix.keys():
-            matrix[tree_name][attribute] = (matrix[tree_name][attribute] - mean) / standard_deviation
-            print("res:", matrix[tree_name][attribute])
-    _dump_matrix(matrix=matrix, output_path=output_path, dialect=dialect)
+        for tree_name in database.keys():
+            database[tree_name][attribute] = (database[tree_name][attribute] - mean) / standard_deviation
+            print("res:", database[tree_name][attribute])
+    return database
 
 
-def _load_matrix(file_path: str, dialect: Dialect) -> Dict[str, Dict[str, float]]:
-    """ Load the matrix located at `file_path` then return a dictionary mapping a tree name with all the attributes, and
-    these attributes to the value given by the tree.
+def _load_database(file_path: str, dialect: Dialect) -> Dict[str, Dict[str, float]]:
+    """ Load the database located at `file_path` then return a dictionary mapping a tree name with all the attributes,
+    and these attributes to the value given by the tree.
     """
-    # matrix = {tree_name: OrderedDict() for tree_name in get_column(path=file_path, column=0, have_header=True,
-    #                                                                dialect=dialect)}
-    matrix = dict()
+    # database = {tree_name: OrderedDict() for tree_name in get_column(path=file_path, column=0, have_header=True,
+    #                                                                  dialect=dialect)}
+    database = dict()
     for row in iter_rows_dict(path=file_path, dialect=dialect):
         tree_name = row[KEY_IDENTIFIER]
         del row[KEY_IDENTIFIER]
-        matrix[tree_name] = OrderedDict(row)
-    return matrix
+        database[tree_name] = OrderedDict(row)
+    return database
 
 
 def _get_attributes(file_path: str, dialect: Dialect) -> List[str]:
@@ -51,25 +50,10 @@ def _get_attributes(file_path: str, dialect: Dialect) -> List[str]:
     return get_header(path=file_path, dialect=dialect)[1:]
 
 
-def _compute_mean(matrix: Dict[str, Dict[str, float]], attribute: str) -> float:
+def _compute_mean(database: Dict[str, Dict[str, float]], attribute: str) -> float:
     """ Compute the mean of all values given by the trees for one attribute """
-    return np.mean([matrix[tree_name][attribute] for tree_name in matrix.keys()])
+    return np.mean([database[tree_name][attribute] for tree_name in database.keys()])
 
 
-def _compute_standard_deviation(matrix: Dict[str, Dict[str, float]], attribute: str) -> float:
-    return np.std([matrix[tree_name][attribute] for tree_name in matrix.keys()])
-
-
-def _dump_matrix(matrix: Dict[str, Dict[str, float]], output_path: str, dialect: Dialect) -> None:
-    """ Dump the normalized matrix. """
-    # Construct header
-    content = [[KEY_IDENTIFIER] + list(list(matrix.values())[0].keys())]
-
-    # Construct content
-    for tree_name in matrix.keys():
-        content.append([tree_name] + list(matrix[tree_name].values()))
-
-    # Dump content
-    dump_csv_content(path=output_path,
-                     content=content,
-                     dialect=dialect)
+def _compute_standard_deviation(database: Dict[str, Dict[str, float]], attribute: str) -> float:
+    return np.std([database[tree_name][attribute] for tree_name in database.keys()])
