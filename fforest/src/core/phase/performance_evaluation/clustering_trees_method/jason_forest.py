b9ba9ec0590e73920273aaca9f05eb3c1d4c4bd3
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-27 19:07:45 +0200

    Not dumping the """matrix""" anymore (keep the database loaded in memory)

--- fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
+++ fforest/src/core/phase/performance_evaluation/clustering_trees_method/jason_forest.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Union
 
-from fforest.src.file_tools.csv_tools import get_header, dump_csv_content, iter_rows_dict
+from fforest.src.file_tools.csv_tools import get_header, iter_rows_dict
 from fforest.src.file_tools.dialect import Dialect
 import fforest.src.getters.environment as env
 import numpy as np
@@ -13,12 +13,11 @@ def jason_forest() -> Dict[str, Dict[str, List[Dict[str, Union[str, int]]]]]:
     for class_name in env.possible_classes:
         for tnorm_name in env.t_norms_names:
             database = _data_normalization(file_path=env.classes_matrices_files_paths[class_name][tnorm_name],
-                                           output_path=env.clustering_trees_files_paths[class_name][tnorm_name],
                                            dialect=env.dialect_output)
     return clustering_trees
 
 
-def _data_normalization(file_path: str, output_path: str, dialect: Dialect) -> Dict[str, Dict[str, float]]:
+def _data_normalization(file_path: str, dialect: Dialect) -> Dict[str, Dict[str, float]]:
     database = _load_database(file_path=file_path, dialect=dialect)
     for attribute in _get_attributes(file_path=file_path, dialect=dialect):
         mean = _compute_mean(database=database, attribute=attribute)
@@ -35,8 +34,6 @@ def _load_database(file_path: str, dialect: Dialect) -> Dict[str, Dict[str, floa
     """ Load the database located at `file_path` then return a dictionary mapping a tree name with all the attributes,
     and these attributes to the value given by the tree.
     """
-    # database = {tree_name: OrderedDict() for tree_name in get_column(path=file_path, column=0, have_header=True,
-    #                                                                  dialect=dialect)}
     database = dict()
     for row in iter_rows_dict(path=file_path, dialect=dialect):
         tree_name = row[KEY_IDENTIFIER]
@@ -56,4 +53,5 @@ def _compute_mean(database: Dict[str, Dict[str, float]], attribute: str) -> floa
 
 
 def _compute_standard_deviation(database: Dict[str, Dict[str, float]], attribute: str) -> float:
+    """ Compute the standard deviatio of all values given by the trees for one attribute """
     return np.std([database[tree_name][attribute] for tree_name in database.keys()])
